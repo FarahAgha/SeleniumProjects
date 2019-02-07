e3539b5f22948e2c3cbcd131a9db9d1578e4d4e8
farahagha <hellotofarah@gmail.com>
2019-02-07 13:17:52 -0600

    Pusing the test code, version 1

--- googlehelphelloworld/src/com/googlehelp/helloworld/ConfigFileReader.java
+++ googlehelphelloworld/src/com/googlehelp/helloworld/ConfigFileReader.java
@@ -6,34 +6,35 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
- 
 public class ConfigFileReader {
- 
- private Properties properties;
- private final String propertyFilePath= "C:\\Users\\deltaman\\Documents\\Automation\\SeleniumProjects\\googlehelphelloworld\\src\\com\\googlehelp\\helloworld\\SearchText.properties";
 
- 
- public ConfigFileReader(){
-	 BufferedReader reader;
-	 try {
-		 reader = new BufferedReader(new FileReader(propertyFilePath));
-		 properties = new Properties();
-		 try {
-			  properties.load(reader);
-			  reader.close();
-			  } catch (IOException e) {
-				  e.printStackTrace();
-			  }
-	 } catch (FileNotFoundException e) {
-		 e.printStackTrace();
-		 throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
-	 } 
- }
- 
-	 public String getSearchText() {
-		 String searchText = properties.getProperty("strSearch");
-		 if(searchText != null) 
-			 return searchText;
-		 else throw new RuntimeException("url not specified in the Configuration.properties file.");
-	 }
+	private Properties properties;
+	private final String propertyFilePath = "C:\\Users\\deltaman\\Documents\\Automation\\SeleniumProjects\\googlehelphelloworld\\src\\com\\googlehelp\\helloworld\\SearchText.properties";
+
+	public ConfigFileReader() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(propertyFilePath));
+			properties = new Properties();
+			try {
+				properties.load(reader);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Configuration.properties not found at "
+					+ propertyFilePath);
+		}
+	}
+
+	public String getSearchText() {
+		String searchText = properties.getProperty("strSearch");
+		if (searchText != null)
+			return searchText;
+		else
+			throw new RuntimeException(
+					"url not specified in the Configuration.properties file.");
+	}
 }
